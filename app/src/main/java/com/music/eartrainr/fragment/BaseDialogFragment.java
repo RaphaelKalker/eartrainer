fc288bbe36f3858cbe669ee14d832ffb671bab1b
Raphael <raphael+kalker+gitlab@gmail.com>
2016-02-26 17:58:43 -0500

    add progress spinner to base dialog fragment

--- app/src/main/java/com/music/eartrainr/fragment/BaseDialogFragment.java
+++ app/src/main/java/com/music/eartrainr/fragment/BaseDialogFragment.java
@@ -3,11 +3,19 @@ package com.music.eartrainr.fragment;
 import android.app.Activity;
 import android.net.Uri;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.music.eartrainr.R;
+
+import butterknife.Bind;
 
 
-public class BaseDialogFragment extends DialogFragment implements FragmentNavigation {
+public class BaseDialogFragment extends DialogFragment implements FragmentNavigation, FragmentProgress {
   public static final String KEY_FRAGMENT_URI_ARG = "fragment_uri";
-  private FragmentNavigation mNavigationCallback;
+  public FragmentNavigation mNavigationCallback;
+
+  @Bind(R.id.progress_spinner) ProgressBar mProgressBar;
 
 
   @Override public void onAttach(final Activity activity) {
@@ -26,11 +34,23 @@ public class BaseDialogFragment extends DialogFragment implements FragmentNaviga
   }
 
   @Override public void onFragmentInteraction(final Uri uri) {
-
+    //TODO: remove this!
   }
 
   @Override public Uri getUri() {
     return getArguments() != null ? (Uri) getArguments()
         .getParcelable(KEY_FRAGMENT_URI_ARG) : Uri.EMPTY;
+  }
+
+  @Override public void showProgress() {
+    if (mProgressBar != null) {
+      mProgressBar.setVisibility(View.VISIBLE);
+    }
+  }
+
+  @Override public void hideProgress() {
+    if (mProgressBar != null) {
+      mProgressBar.setVisibility(View.GONE);
+    }
   }
 }
