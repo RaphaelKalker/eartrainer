a3e92b07a1f72544d11092615ebe1301975243ba
Raphael <raphael+kalker+gitlab@gmail.com>
2016-03-06 16:01:24 -0500

    add font awesome

--- app/src/main/java/com/music/eartrainr/EarTrainerApplication.java
+++ app/src/main/java/com/music/eartrainr/EarTrainerApplication.java
@@ -3,6 +3,8 @@ package com.music.eartrainr;
 import android.app.Application;
 
 import com.firebase.client.Firebase;
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 import butterknife.ButterKnife;
 
@@ -13,6 +15,7 @@ public class EarTrainerApplication extends Application {
     super.onCreate();
     initFirebase();
     ButterKnife.setDebug(false);
+    Iconify.with(new FontAwesomeModule());
 
   }
 
