8d0aca6b9941277dcd3c28dcdbb39580610401d6
Raphael <raphael+kalker+gitlab@gmail.com>
2016-03-08 15:48:22 -0500

    add NavigationEvent

---
+++ app/src/main/java/com/music/eartrainr/event/NavigationEvent.java
@@ -0,0 +1,24 @@
+package com.music.eartrainr.event;
+
+import com.music.eartrainr.Auth;
+
+import static com.music.eartrainr.event.NavigationEvent.EVENT.NAVIGATION_FAILED;
+
+
+public class NavigationEvent extends EventBusEvent {
+
+  public static final String TAG = NavigationEvent.class.getSimpleName();
+
+
+  public interface EVENT {
+    int NAVIGATION_FAILED = Auth.generateEventToken(TAG, "navigation_failed");
+  }
+
+
+  public NavigationEvent error(final String target, final String error) {
+    mEventID = NAVIGATION_FAILED;
+    mData = target;
+    mError = error;
+    return this;
+  }
+}
