7c8f05c1644332faede829e65de16bf8c8c379b4
Raphael <raphael+kalker+gitlab@gmail.com>
2016-03-13 16:53:39 -0400

    add placeholder for game fragment

--- app/src/main/java/com/music/eartrainr/fragment/GameFragment.java
+++ app/src/main/java/com/music/eartrainr/fragment/GameFragment.java
@@ -1,4 +1,31 @@
 package com.music.eartrainr.fragment;
 
+import android.util.SparseArray;
+
+
 public abstract class GameFragment extends BaseFragment {
+
+  SparseArray<Game> mGames = new SparseArray();
+
+  class Game {
+
+
+    private int midiFile;
+    private int task;
+
+
+    public int getMidiFile() {
+      return midiFile;
+    }
+
+    public void setMidiFile(final int midiFile) {
+      this.midiFile = midiFile;
+    }
+
+  }
+
+
+  private Game getNextGame(final int gameSessionID) {
+    return mGames.get(gameSessionID);
+  }
 }
