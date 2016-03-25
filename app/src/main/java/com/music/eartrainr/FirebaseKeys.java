7c99d4ff9ca6b9e7275ce5bbd6644c0fd5d419fc
Raphael <raphael.kalker@gmail.com>
2016-03-25 17:06:15 -0400

    firebase keys for games

---
+++ app/src/main/java/com/music/eartrainr/FirebaseKeys.java
@@ -0,0 +1,11 @@
+package com.music.eartrainr;
+
+/**
+ * Created by Raphael on 16-03-25.
+ */
+public interface FirebaseKeys {
+  String GAMES = "games";
+  String INTERVAL_DETECTION = "interval_detection";
+  String ANSWER = "answer";
+  String MIDI = "midi";
+}
