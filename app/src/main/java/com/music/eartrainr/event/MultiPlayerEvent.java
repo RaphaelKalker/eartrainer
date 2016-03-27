6c312978d2f18162125095706b72368b8ad5a2bd
Raphael <raphael.kalker@gmail.com>
2016-03-27 01:02:33 -0400

    initialize "prepare game state"

--- app/src/main/java/com/music/eartrainr/event/MultiPlayerEvent.java
+++ app/src/main/java/com/music/eartrainr/event/MultiPlayerEvent.java
@@ -1,15 +1,17 @@
 package com.music.eartrainr.event;
 
+import com.music.eartrainr.api.MultiplayerService;
 import com.music.eartrainr.model.MultiplayerGame;
 import com.music.eartrainr.utils.Auth;
 
 
-public class MultiPlayerEvent extends EventBusEvent<MultiplayerGame> {
+public class MultiPlayerEvent extends EventBusEvent {
 
   public static final String TAG = MultiPlayerEvent.class.getSimpleName();
 
   public interface EVENT {
     int MATCH_REQUEST_SUCCESS = Auth.generateEventToken(TAG, "match_request_success");
+    int MATCH_PREPARE_PENDING = Auth.generateEventToken(TAG, "match_prepare_pending");
   }
 
   public MultiPlayerEvent success(final MultiplayerGame multiplayerGame) {
@@ -21,6 +23,12 @@ public class MultiPlayerEvent extends EventBusEvent<MultiplayerGame> {
   public MultiPlayerEvent error(final MultiplayerGame multiplayerGame) {
     mEventID = EVENT.MATCH_REQUEST_SUCCESS;
     mData = multiplayerGame;
+    return this;
+  }
+
+  public MultiPlayerEvent prepareStart(final String message) {
+    mEventID = EVENT.MATCH_PREPARE_PENDING;
+    mData = message;
     return this;
   }
 
--- app/src/main/java/com/music/eartrainr/gcm/GcmNotificationReceiver.java
+++ app/src/main/java/com/music/eartrainr/gcm/GcmNotificationReceiver.java
@@ -4,9 +4,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 
-import com.music.eartrainr.GameManager;
 import com.music.eartrainr.Wtf;
 import com.music.eartrainr.activity.IntervalDetectionGameActivity;
 
@@ -17,7 +15,7 @@ public class GcmNotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        boolean matchResponse = intent.getBooleanExtra(GcmIntentService.notificationAction.MATCH_RESPONSE, false);
+        boolean matchResponse = intent.getBooleanExtra(GcmIntentService.ACTION.MATCH_RESPONSE, false);
         if (!matchResponse) {
             //TODO notify API that things are cancelled (Send GCM to other player to let them know)
 
@@ -32,7 +30,7 @@ public class GcmNotificationReceiver extends BroadcastReceiver {
 
     private void cancelNotification (Context context) {
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.cancel(GcmIntentService.notificationAction.NOTIFICATION_ID);
+        manager.cancel(GcmIntentService.ACTION.NOTIFICATION_ID);
         Intent closeDialogIntent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         context.sendBroadcast(closeDialogIntent);
     }
