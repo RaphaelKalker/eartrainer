bdba4b6904da6b52ade3128097eb9490483ffafc
Raphael <raphael.kalker@gmail.com>
2016-03-28 19:29:13 -0400

    view cleanup

--- app/src/main/java/com/music/eartrainr/EarTrainerApplication.java
+++ app/src/main/java/com/music/eartrainr/EarTrainerApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.firebase.client.Firebase;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.joanzapata.iconify.fonts.IoniconsModule;
 
 import butterknife.ButterKnife;
 
@@ -15,7 +16,7 @@ public class EarTrainerApplication extends Application {
     super.onCreate();
     initFirebase();
     ButterKnife.setDebug(false);
-    Iconify.with(new FontAwesomeModule());
+    Iconify.with(new FontAwesomeModule()).with(new IoniconsModule());
 
   }
 
